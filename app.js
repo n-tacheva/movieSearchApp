c2219a46cd940e6a61264486a37d6b7252891ef7
Nadejda Tacheva <nadejdatacheva@gmail.com>
2020-08-17 01:11:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000
+let request = require("request")
+app.set("view engine", "ejs")
+
+
+app.get("/", (req, res)=>{
+    res.render("search")
+
+})
+
+app.get("/results", (req,res)=> {
+    let query = req.query.search
+    let url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`
+    request (url, (error, response, body)=> {
+        if (!error && response.statusCode == 200) {
+            let data = JSON.parse(body)
+            if(data.Error) {
+                console.log(data)
+                return
+            }
+            res.render("results", data)
+            
+        }
+    });
+    
+})
+
+app.listen(port, ()=>{
+    console.log(`App is up and running on port ${port}`)
+})
